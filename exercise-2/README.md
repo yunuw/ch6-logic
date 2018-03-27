7d0dcb763102f11ec34d1405084b7a5aa3d94d08
Joel Ross <joelross@uw.edu>
2018-03-27 09:00:33 -0700

    Add exercise solutions

--- exercise-2/README.md
+++ exercise-2/README.md
@@ -4,8 +4,41 @@ In this exercise, you'll practice writing conditional statements in Python. In p
 
 1. [no_teen_sum](http://codingbat.com/prob/p100347)
 
+	```python
+	#solution
+	def no_teen_sum(a, b, c):
+		return fix_teen(a) + fix_teen(b) + fix_teen(c)
+
+	def fix_teen(n):
+		if 13 <= n <= 19 and n != 15 and n != 16:
+			return 0
+		else:
+			return n
+	```
+
 2. [lucky_sum](http://codingbat.com/prob/p107863)
 
+	```python
+	#solution
+	def lucky_sum(a, b, c):
+		if a == 13:
+			return 0
+		elif b == 13:
+			return a
+		elif c == 13:
+			return a+b
+		else:
+			return a+b+c
+	```
+
 3. [make_brick](http://codingbat.com/prob/p118406). Note that you may not need to use conditional statements for this, but it's good practice at the kind of logical, abstract thinking used in programming!
+
+	```python
+	#solution
+	def make_bricks(small, big, goal):
+  		num_big = min(goal//5,big)
+  		num_small = goal - num_big*5
+  		return num_small <= small
+  	```
 
 Feel free to practice with the other problems as well!
--- exercise-3/exercise.py
+++ exercise-3/exercise.py
@@ -17,7 +17,22 @@
 # Be sure to consider both positive AND negative distances!
 # BONUS: Also print out whether the guess is high or low
 
-
+def print_hot_or_cold(target, guess):
+    dist = abs(target - guess)
+    if dist == 0:
+        print("got it!")
+    elif dist <= 1:
+        print("scalding hot")
+    elif dist <= 3:
+        print("very warm")
+    elif dist <= 5:
+        print("warm")
+    elif dist <= 8:
+        print("cold")
+    elif dist <= 13:
+        print("very cold")
+    else:
+        print("icy freezing miserably cold")
 
 # Define a function `guess_number()` that takes a single argument (a target number)
 # and prompts the user for a guess using the `input()` method. Your function should
@@ -29,8 +44,17 @@
 # or by simply calling your `guess_number() method again IF the user didn't get
 # the answer right. The later is an example of **recursion**.
 
-
+def guess_number(target):
+    guess = int(input("Guess a number! "))
+    print_hot_or_cold(target, guess)
+    if target != guess:
+        guess_number(target)
 
 # If the file is run as a top-level script, your script should pick a random number
 # between 1 and 50 as the target and then start the game. You should inform the
 # user of the range of numbers before asking them for a guess.
+if __name__ == "__main__":
+    from random import randint
+    target = randint(1, 50)
+    print("I picked a number between 1 and 50. Try and guess!")
+    guess_number(target)  # call the guess function
